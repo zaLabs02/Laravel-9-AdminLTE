149125e48569ccf01333bf4e7fd575bf61573501
afrizal423 <afrizal2499@gmail.com>
2022-01-16 05:13:15 +0700

    sync laravel/laravel dev-master

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,9 +1,5 @@
 window._ = require('lodash');
 
-try {
-    require('bootstrap');
-} catch (e) {}
-
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
